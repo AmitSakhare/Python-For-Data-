2f96288e14afb29d0759d8e1f14569d2bb9459c2
Amit <amit.sakhare37@gmail.com>
2024-06-25 14:47:20 +0530

    Variables & Data Types

--- Variable & Data Types.py
+++ Variable & Data Types.py
@@ -1,7 +1,7 @@
 #Data Types - Data types are used to define  what type of data we are going to sotre in a variable.
 # Types of Data type in Python
 '''
-1. Numeric type
+1. Numeric type - Python numeric data types store numeric values.
  - Integers 
  - Floats 
  - Complex 
@@ -17,3 +17,25 @@
 4. Set
 
 '''
+
+#1. Numeric type - Python numeric data types store numeric values.
+#Examples of Numeric Data Types
+# Integer Numeric Data types
+intNum =25
+print(intNum," is ",type(intNum))
+# Output :- 25  is  <class 'int'>
+
+# Float Numeric Data types
+floatNum =17.254
+print(floatNum , " is ",type(floatNum))
+# Output :- 17.254  is  <class 'float'>
+
+# Complex Numeric Data types
+complexNum =21+5j
+print(complexNum, " is ", type(complexNum))
+# Output :-(21+5j)  is  <class 'complex'>
+
+# Boolean Numeric Data types
+BooleanIsmale = True
+print(BooleanIsmale, " is ", type(BooleanIsmale))
+# Output :- True is <class 'bool'>
