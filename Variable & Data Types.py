62196ba5342473faa16e6822dc6fef6ee472245c
Amit <amit.sakhare37@gmail.com>
2024-06-25 12:40:22 +0530

    Variables & Data Types

---
+++ Variable & Data Types.py
@@ -0,0 +1,19 @@
+#Data Types - Data types are used to define  what type of data we are going to sotre in a variable.
+# Types of Data type in Python
+'''
+1. Numeric type
+ - Integers 
+ - Floats 
+ - Complex 
+ - Boolean
+
+2. Dictionary - 
+
+3. Sequence Type
+- List
+- Tuple
+- String
+
+4. Set
+
+'''
